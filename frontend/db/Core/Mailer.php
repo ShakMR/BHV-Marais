aea4fc74b46aa4418fda4dfe1d8109c2e7f337c7
ShakMR <borja.arias.upc@gmail.com>
2015-11-26 22:49:32 +0100

    Fixed mail for the winner

--- frontend/db/Core/Mailer.php
+++ frontend/db/Core/Mailer.php
@@ -94,7 +94,9 @@ class Mailer
     public function sendMail() {
         $subject = $this->type->subject();
         $aux_mails = $this->type->mail_array();
+        echo $aux_mails;
         $mails = $this->user_mail;
+        echo $mails;
         if ($aux_mails)
             $mails = $aux_mails;
         $from = $this->type->from();
