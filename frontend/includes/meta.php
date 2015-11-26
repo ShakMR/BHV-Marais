ff91aeabd8cb8b5f22c0a9ae39ecff61ed1be2b2
ShakMR <borja.arias.upc@gmail.com>
2015-11-26 21:06:26 +0100

    Fixed mail for the winner

--- frontend/includes/meta.php
+++ frontend/includes/meta.php
@@ -4,5 +4,6 @@
 	<meta charset="UTF-8">
 	<title>Formulario Navidad</title>
 	<link rel="stylesheet" href="css/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 </head>
