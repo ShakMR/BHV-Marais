5446a7794d5d7cf05286201f66f406df1f09fdc5
ShakMR <borja.arias.upc@gmail.com>
2015-11-26 21:20:13 +0100

    Fixed mail for the winner

--- frontend/includes/login-form.php
+++ frontend/includes/login-form.php
@@ -1,15 +1,18 @@
-
 <body>
+<div class="img-wrapper">
+    <img class="back-img back-img-g"src="img/paraplui.jpg">
+</div>
 <form class="login-form" action="index.php" method="post" autocomplete="off">
-	<section class="form-block form-block-info">
-		<img class="form-logo" src="img/logoNoel.jpg">
-		<p class="form-note">
-			LOG IN pour commencer le jeu. (Appelez au service)
-		</p>
-		<input class="form-field form-code" name="username" type="text" placeholder="USERNAME">
-		<input class="form-field" name="password" type="password" placeholder="PASSWORD">
-		<input class="form-field form-submit" name="submit" type="submit" value="LOG IN">
-	</section>
+    <section class="form-block form-block-info">
+        <img class="form-logo" src="img/logoNoel.jpg">
+        <p class="form-note">
+            LOG IN pour commencer le jeu. (Merci de vous rapprocher de l'hôtesse au welcomedesk)
+        </p>
+        <input class="form-field form-code" name="username" type="text" placeholder="USERNAME">
+        <input class="form-field" name="password" type="password" placeholder="PASSWORD">
+        <input class="form-field form-submit" name="submit" type="submit" value="LOG IN">
+    </section>
 </form>
-<img class="back-img back-img-d"src="img/papanoel.jpg">
-<img class="back-img back-img-g"src="img/paraplui.jpg">
+<div class="img-wrapper">
+    <img class="back-img back-img-d"src="img/papanoel.jpg">
+</div>
--- frontend/includes/meta.php
+++ frontend/includes/meta.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Formulario Navidad</title>
-	<link rel="stylesheet" href="css/styles.css">
+	<title>Formulario Navidad</title><!-- <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'> -->
+    <link href='https://fonts.googleapis.com/css?family=Alice' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 </head>
